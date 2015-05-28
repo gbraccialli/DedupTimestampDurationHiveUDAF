5548da167bf0e858e23375d84b0b29a90fb2f63b
gbraccialli <gbraccialli@gmail.com>
2015-05-27 22:38:28 -0300

    create function udfs

--- scripts/create_function.sql
+++ scripts/create_function.sql
@@ -2,3 +2,11 @@
 ADD JAR /jars/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar;
 CREATE TEMPORARY FUNCTION dedup_timestamp_duration as 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF';
 SET hive.map.aggr=false;
+
+
+OR
+
+CREATE TEMPORARY FUNCTION dedup_timestamp_duration AS 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF' USING JAR 'hdfs:///jars/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar'
+SET hive.map.aggr=false;
+
+
