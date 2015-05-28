04005c6df7b050aa15fa0da0da69c545f402d113
gbraccialli <gbraccialli@gmail.com>
2015-05-27 21:17:38 -0300

    correction url for .jar

--- scripts/create_function.sql
+++ scripts/create_function.sql
@@ -1,4 +1,4 @@
---wget https://github.com/gbraccialli/GeohashHiveUDF/raw/master/target/GeohashHiveUDF-1.0-SNAPSHOT-jar-with-dependencies.jar
+--wget https://github.com/gbraccialli/DedupTimestampDurationHiveUDAF/raw/master/target/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar
 ADD JAR /jars/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar;
 CREATE TEMPORARY FUNCTION dedup_timestamp_duration as 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF';
 SET hive.map.aggr=false;
