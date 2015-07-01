2f0708a45b875920a6b32bc49ec299efca1e3af0
gbraccialli <gbraccialli@gmail.com>
2015-07-01 01:40:11 -0300

    using auxlib for jars and create function (not temporary)

--- scripts/create_function.sql
+++ scripts/create_function.sql
@@ -1,12 +1,11 @@
---wget https://github.com/gbraccialli/DedupTimestampDurationHiveUDAF/raw/master/target/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar
-ADD JAR /jars/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar;
-CREATE TEMPORARY FUNCTION dedup_timestamp_duration as 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF';
+unix
+wget https://github.com/gbraccialli/DedupTimestampDurationHiveUDAF/raw/master/target/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar
+mkdir /usr/hdp/current/hive-server2/auxlib
+cp DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar /usr/hdp/current/hive-server2/auxlib
+--restart hive from ambari
+
+hive or beeline:
+
+CREATE FUNCTION dedup_timestamp_duration as 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF';
+CREATE FUNCTION simple_phonenumber_bucket as 'com.github.gbraccialli.hive.udf.SimplePhoneNumberBucket';
 SET hive.map.aggr=false;
-
-
-OR
-
-CREATE TEMPORARY FUNCTION dedup_timestamp_duration AS 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF' USING JAR 'hdfs:///jars/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar'
-SET hive.map.aggr=false;
-
-
