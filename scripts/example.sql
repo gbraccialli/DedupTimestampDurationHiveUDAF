249e649aa3b0c23b828f3588459294115f587e92
gbraccialli <gbraccialli@gmail.com>
2015-07-01 01:41:04 -0300

    new function simple_phonenumber_bucket

--- scripts/example.sql
+++ scripts/example.sql
@@ -1,8 +1,9 @@
 !connect jdbc:hive2://localhost:10000/default
+!connect jdbc:hive2://seregiondev02.cloud.hortonworks.com:10000/default
 
-CREATE TEMPORARY FUNCTION dedup_timestamp_duration AS 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF' USING JAR 'hdfs:///jars/DedupTimeStampDurationHiveUDAF-1.0-SNAPSHOT-jar-with-dependencies.jar';
-
-set hive.map.aggr=false;
+CREATE TEMPORARY FUNCTION dedup_timestamp_duration as 'com.github.gbraccialli.hive.udaf.DedupTimestampDurationHiveUDAF';
+CREATE TEMPORARY FUNCTION simple_phonenumber_bucket as 'com.github.gbraccialli.hive.udf.SimplePhoneNumberBucket';
+SET hive.map.aggr=false;
 
 SELECT inline(record) as (date,time,duration,cdr_type,num_a,num_b,central,erb,imsi,emei,other1,other2,other3)
 FROM (
@@ -17,25 +18,29 @@ FROM (
 ORDER BY date, time;
 
 
-
-SELECT inline(record) as (date,time,duration,cdr_type,num_a,num_b,central,erb,imsi,emei,other1,other2,other3)
-FROM (
-  SELECT 
-    dedup_timestamp_duration(
-      unix_timestamp(concat(date, ' ', time),'yyyyMMdd HHmmss'),duration,cdr_type,struct(date,time,duration,cdr_type,num_a,num_b,central,erb,imsi,emei,other1,other2,other3)
-    ) as record 
-  FROM cdr_partitioned
-  WHERE hash_target = (abs(hash('11981341618'))  % 11) AND
-  date BETWEEN 20150102 AND 20150107 AND
-  (num_a = '11981341618' or num_b = '11981341618')
-  GROUP BY num_a, num_b
-) t1
-ORDER BY date, time;
+SELECT * FROM (
+  SELECT inline(record) as (date,time,duration,cdr_type,num_a,num_b,central,erb,imsi,emei,other1,other2,other3)
+  FROM (
+    SELECT 
+      dedup_timestamp_duration(
+        unix_timestamp(concat(date, ' ', time),'yyyyMMdd HHmmss'),duration,cdr_type,
+        struct(date,time,duration,cdr_type,num_a,num_b,central,erb,imsi,emei,other1,other2,other3)
+      ) as record 
+    FROM cdr_partitioned
+    WHERE hash_target = simple_phonenumber_bucket('11981341618',5) AND
+    date BETWEEN 20150102 AND 20150107 AND
+    (num_a = '11981341618' or num_b = '11981341618')
+    GROUP BY num_a, num_b
+  ) t1
+) t2  
+DISTRIBUTE BY num_a, num_b
+SORT BY num_a, num_b, date, time;
 
 
 SELECT distinct num_a, date
 FROM cdr_partitioned
 WHERE erb = 'xx'
+;
 
 SELECT /*+ MAPJOIN(erb) */
   cdr.* 
@@ -46,7 +51,7 @@ INNER JOIN (
   WHERE erb = 'xx'
 ) erb ON
   cdr.date = erb.date AND
-  cdr.hash_target = (abs(hash(erb.num_a))  % 11)
+  cdr.hash_target = simple_phonenumber_bucket(erb.num_a,5)
 WHERE
   cdr.direction = 'a-b'
 ;
@@ -57,10 +62,10 @@ SELECT inline(record) as (date,time,duration,cdr_type,num_a,num_b,central,erb,im
 FROM (
   SELECT 
     dedup_timestamp_duration(
-      unix_timestamp(concat(date, ' ', time),'yyyyMMdd HHmmss'),duration,cdr_type,struct(date,time,duration,cdr_type,num_a,num_b,central,erb,imsi,emei,other1,other2,other3)
+      unix_timestamp(concat(date, ' ', time),'yyyyMMdd HHmmss'),duration,cdr_type,
+      struct(date,time,duration,cdr_type,num_a,num_b,central,erb,imsi,emei,other1,other2,other3)
     ) as record 
   FROM (
-  
       SELECT /*+ MAPJOIN(erb) */
         cdr.* 
       FROM cdr_partitioned cdr
@@ -70,10 +75,13 @@ FROM (
         WHERE erb = 'xx'
       ) erb ON
         cdr.date = erb.date AND
-        cdr.hash_target = (abs(hash(erb.num_a))  % 11)
+        cdr.hash_target = simple_phonenumber_bucket(erb.num_a,5)
       WHERE
         cdr.direction = 'a-b'
   
   ) t1
   GROUP BY num_a, num_b
 ) t2;
+
+
+
